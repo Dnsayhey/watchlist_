31993d0a5f472becf6f2a7190d37024ffcee778a
dnsayhey <yanlei921@live.com>
2024-02-19 15:24:51 +0800

    Add favicon, base template and error template.

--- app.py
+++ app.py
@@ -67,6 +67,17 @@ class Movie(db.Model):
     year: Mapped[str] = mapped_column(String(4))
 
 
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
 @app.route("/hello")
 def hello():
     return "<h1>Hello!</h1>"
@@ -74,4 +85,5 @@ def hello():
 
 @app.route("/")
 def index():
-    return render_template("index.html", name=name, movies=movies)
+    movies = Movie.query.all()
+    return render_template("index.html", movies=movies)
