b0ddd552a3880dea5fabc7bc24c381d48acd4d47
dnsayhey <yanlei921@live.com>
2024-02-19 14:52:40 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,19 +1,70 @@
-from flask import Flask, render_template
+import os
+import sys
 
-name = "Zero"
-movies = [
-    {"title": "My Neighbor Totoro", "year": "1988"},
-    {"title": "Dead Poets Society", "year": "1989"},
-    {"title": "A Perfect World", "year": "1993"},
-    {"title": "Leon", "year": "1994"},
-    {"title": "Mahjong", "year": "1996"},
-    {"title": "Swallowtail Butterfly", "year": "1996"},
-    {"title": "King of Comedy", "year": "1999"},
-    {"title": "Devils on the Doorstep", "year": "1999"},
-    {"title": "WALL-E", "year": "2008"},
-    {"title": "The Pork of Music", "year": "2012"},
-]
+import click
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+if sys.platform.startswith("win"):
+    prefix = "sqlite:///"
+else:
+    prefix = "sqlite:////"
+
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = prefix + os.path.join(app.root_path, "data.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+@app.cli.command()
+@click.option("--drop", is_flag=True, help="Create after drop.")
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("Initialized database.")
+
+
+@app.cli.command()
+def forge():
+    db.create_all()
+
+    name = "Zero"
+    movies = [
+        {"title": "My Neighbor Totoro", "year": "1988"},
+        {"title": "Dead Poets Society", "year": "1989"},
+        {"title": "A Perfect World", "year": "1993"},
+        {"title": "Leon", "year": "1994"},
+        {"title": "Mahjong", "year": "1996"},
+        {"title": "Swallowtail Butterfly", "year": "1996"},
+        {"title": "King of Comedy", "year": "1999"},
+        {"title": "Devils on the Doorstep", "year": "1999"},
+        {"title": "WALL-E", "year": "2008"},
+        {"title": "The Pork of Music", "year": "2012"},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m["title"], year=m["year"])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo("Done.")
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(28))
+
+
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(60))
+    year: Mapped[str] = mapped_column(String(4))
 
 
 @app.route("/hello")
